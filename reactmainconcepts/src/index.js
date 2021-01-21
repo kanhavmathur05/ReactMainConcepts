ab70707c3f8d13d9c8bbdac9b39991ad3d5dc6af
Kanhav Mathur <kanhavmathur05@gmail.com>
2021-01-21 23:11:13 +0530

    Components and props 2

--- reactmainconcepts/src/index.js
+++ reactmainconcepts/src/index.js
@@ -16,15 +16,15 @@ import reportWebVitals from './reportWebVitals';
 
 // const element=(<h1 className="greeting"> Hello, {formatname(user)}!!</h1>);
 
-function Welcome(props) {
-  return (<h1>Hello, {props.name}</h1>)
-}
-
-// class Welcome extends React.Component {
-//   render() {
-//     return <h1>Hello, {this.props.name}!!</h1>
-//   }
+// function Welcome(props) {
+//   return (<h1>Hello, {props.name}</h1>)
 // }
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}!!</h1>
+  }
+}
 
 const element = <Welcome name="Goku" />
 
