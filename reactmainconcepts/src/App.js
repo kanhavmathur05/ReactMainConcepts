a3986ddcb8e76bfb895aa552baf2ae396fa7ed23
Kanhav Mathur <kanhavmathur05@gmail.com>
2021-01-31 13:37:59 +0530

    Components Rendering implemented

--- reactmainconcepts/src/App.js
+++ reactmainconcepts/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}!!</h1>
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div>
+            <Welcome name="Goku"/>
+            <Welcome name="Echidna"/>
+            <Welcome name="Stella"/>
+            <Welcome name="Vegeta"/>
+          </div>    
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
