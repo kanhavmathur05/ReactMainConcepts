e27ad88e94ba02bd794f6d7335f0e7630ca4d0a7
Kanhav Mathur <kanhavmathur05@gmail.com>
2021-02-05 22:24:31 +0530

    Lists and Key implemented

--- reactmainconcepts/src/index.js
+++ reactmainconcepts/src/index.js
@@ -206,6 +206,73 @@ class Page extends React.Component {
     );
   }
 }
+function ListItem(props) {
+  return <li>{props.value}</li>;
+}
+
+// function NumberList(props) {
+//   const numbers = props.numbers;
+//   const listItems = numbers.map((number) =>
+//     <ListItem key={number.toString()} value={number}/>
+//     //<li key={number.toString()}>{number}</li>
+//   );
+//   return (
+//    <ul>{listItems}</ul>
+//   );
+// }
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  return (
+    <ul>
+      {numbers.map((number) =>
+      <ListItem key={number.toString()} value={number}/>
+    //<li key={number.toString()}>{number}</li>
+  )}
+  </ul>);
+//  return (
+  // <ul>{listItems}</ul>
+  //);
+}
+
+const numbers=[1,2,3,4,5];
+
+const posts = [
+  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
+function Blog(props)
+{
+  const sidebar=(
+    <ul>
+      {props.posts.map((post) => 
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  );
+
+  const content=(
+    
+      props.posts.map((post) => 
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+        </div>
+      )
+    
+  );
+
+  return (
+    <div>
+      {sidebar}
+      <hr></hr>
+      {content}
+    </div>
+  );
+}
+
 
 //function tick()
 //{
@@ -219,8 +286,10 @@ class Page extends React.Component {
 //      <Greeting isLoggedIn={true}/>,
 //    <LoginControl />,
 //    <MailBox unreadmessages={messages} />,
-    <Page />,
-    document.getElementById('root')
+//    <Page />,
+//      <NumberList numbers={numbers}/>,
+      <Blog posts={posts} />,
+      document.getElementById('root')
  );
 //}
 
